05dc07915a61bf3f8c1827a63240ca53c4aa482d
kahenya-anita <anitakahenya1@gmail.com>
2020-09-28 22:50:14 +0300

    Add user login, logout & register functionality

--- app/models.py
+++ app/models.py
@@ -2,12 +2,13 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from . import db
-# from . import login_manager
+
+from . import login_manager
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
     
 
 class Quote:
@@ -25,6 +26,7 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String)
     prof_pic_path = db.Column(db.String)
     bio = db.Column(db.String)
+    password_hash = db.Column(db.String(255))
 
    @property
     def password(self):
